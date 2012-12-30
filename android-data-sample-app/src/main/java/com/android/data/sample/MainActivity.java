a6d3ab62f9616c8a0cb97cdf5771834bb43045f2
Selva <k.n.selvakumar@gmail.com>
2012-12-30 15:50:25 +0530

    Sample empty app

---
+++ android-data-sample-app/src/main/java/com/android/data/sample/MainActivity.java
@@ -0,0 +1,10 @@
+package com.android.data.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
