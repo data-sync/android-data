d510a84fdc6275d35abac639aab7621226e2e636
Selva <k.n.selvakumar@gmail.com>
2013-01-06 05:28:06 +0530

    Not all tests need to be android service test cases

--- android-data-tests/src/main/java/com/android/data/BaseTestCase.java
+++ android-data-tests/src/main/java/com/android/data/BaseTestCase.java
@@ -1,24 +1,23 @@
 package com.android.data;
 
-import android.content.Intent;
-import android.os.IBinder;
-import android.test.ServiceTestCase;
+import android.test.InstrumentationTestCase;
 import com.android.data.document_for_test.Task;
-import com.android.data.services.DataService;
 
-public class BaseTestCase extends ServiceTestCase<DataService> {
+public class BaseTestCase extends InstrumentationTestCase {
     protected Repository<Task> repository;
-
-    public BaseTestCase() {
-        super(DataService.class);
-    }
+    private DataStore dataStore;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        IBinder binder = bindService(new Intent(getContext(), DataService.class));
-        DataService service = ((DataService.DataServiceBinder) binder).getService();
-        repository = new Repository<Task>(Task.class, service.getDataStore());
+        dataStore = new DataStore(getInstrumentation().getContext(), "test.db");
+        repository = new Repository<Task>(Task.class, dataStore);
         repository.reset();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        dataStore.close();
+        super.tearDown();
     }
 }
