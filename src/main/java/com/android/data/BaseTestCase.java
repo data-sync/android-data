a324b5f832473f58be72e05c9897f924a425ba43
Selva <k.n.selvakumar@gmail.com>
2013-01-06 16:17:10 +0530

    find by complex keys

--- src/main/java/com/android/data/BaseTestCase.java
+++ src/main/java/com/android/data/BaseTestCase.java
@@ -4,15 +4,14 @@ import android.test.InstrumentationTestCase;
 import com.android.data.document_for_test.Task;
 
 public class BaseTestCase extends InstrumentationTestCase {
-    protected Repository<Task> repository;
+    protected Repository<Task> taskRepository;
     private DataStore dataStore;
 
     @Override
     public void setUp() throws Exception {
-        super.setUp();
         dataStore = new DataStore(getInstrumentation().getContext(), "test.db");
-        repository = new Repository<Task>(Task.class, dataStore);
-        repository.reset();
+        taskRepository = new Repository<Task>(Task.class, dataStore);
+        taskRepository.reset();
     }
 
     @Override
