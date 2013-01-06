9bf648f03ae3d7ace599fb67bcc20e5e6806d95d
Selva <k.n.selvakumar@gmail.com>
2013-01-07 01:51:56 +0530

    Moved test adapter to its own class

--- src/main/java/com/android/data/DataListAdapterTest.java
+++ src/main/java/com/android/data/DataListAdapterTest.java
@@ -1,10 +1,8 @@
 package com.android.data;
 
-import android.R;
 import android.test.suitebuilder.annotation.MediumTest;
-import android.view.View;
-import android.widget.TextView;
-import com.android.data.document_for_test.Task;
+import com.android.data.adapters.TaskListAdapter;
+import com.android.data.models.Task;
 import org.ektorp.android.util.CouchbaseViewListAdapter;
 
 public class DataListAdapterTest extends BaseTestCase {
@@ -13,18 +11,6 @@ public class DataListAdapterTest extends BaseTestCase {
     private Task task1;
     private CouchbaseViewListAdapter adapter;
 
-    class TaskListAdapter extends DataListAdapter<Task> {
-        public TaskListAdapter(Repository<Task> repository) {
-            super(repository, "byDescription", R.layout.simple_list_item_1, true);
-        }
-
-        @Override
-        View populateView(View view, Task document) {
-            ((TextView) view).setText(document.getDescription());
-            return view;
-        }
-    }
-
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -32,7 +18,7 @@ public class DataListAdapterTest extends BaseTestCase {
         taskRepository.add(task1);
         taskRepository.defineViewBy("description");
         dataListAdapter = new TaskListAdapter(taskRepository);
-        sleepEnoughForAdapterToRequery();
+        sleepEnoughForAdapterToReQuery();
     }
 
     @MediumTest
@@ -45,11 +31,7 @@ public class DataListAdapterTest extends BaseTestCase {
     public void testFollowChanges() throws InterruptedException {
         assertEquals(1, dataListAdapter.getCount());
         taskRepository.add(new Task("new task"));
-        sleepEnoughForAdapterToRequery();
+        sleepEnoughForAdapterToReQuery();
         assertEquals(2, dataListAdapter.getCount());
-    }
-
-    private void sleepEnoughForAdapterToRequery() throws InterruptedException {
-        Thread.sleep(1000);
     }
 }
