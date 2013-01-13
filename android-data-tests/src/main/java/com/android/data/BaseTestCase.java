94b61aa7e521ec8884542e860580402bf19f564c
Selva <k.n.selvakumar@gmail.com>
2013-01-13 16:04:10 +0530

    Bumping wait time to make the test stable

--- android-data-tests/src/main/java/com/android/data/BaseTestCase.java
+++ android-data-tests/src/main/java/com/android/data/BaseTestCase.java
@@ -22,7 +22,7 @@ public class BaseTestCase extends InstrumentationTestCase {
     }
 
     protected void sleepEnoughForAdapterToReQuery() throws InterruptedException {
-        Thread.sleep(100);
+        Thread.sleep(500);
     }
 
     protected Context getContext() {
