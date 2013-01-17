e75adaceba9222296afd3685ec409998c2e2eb27
Selva <k.n.selvakumar@gmail.com>
2013-01-17 23:40:55 +0530

    Fix to keep the listAdapter change listener to run in ThreadPool, so that they are alive
    TODO: Consider a pull request to CouchbaseViewListAdapter

--- src/main/java/com/android/data/DataListAdapter.java
+++ src/main/java/com/android/data/DataListAdapter.java
@@ -1,14 +1,21 @@
 package com.android.data;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.DbAccessException;
 import org.ektorp.ViewQuery;
 import org.ektorp.ViewResult;
+import org.ektorp.android.util.ChangesFeedAsyncTask;
 import org.ektorp.android.util.CouchbaseViewListAdapter;
+import org.ektorp.android.util.EktorpAsyncTask;
+import org.ektorp.changes.ChangesCommand;
+import org.ektorp.changes.DocumentChange;
 
 import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
@@ -68,6 +75,82 @@ public abstract class DataListAdapter<T extends Document> extends CouchbaseViewL
         ParameterizedType parameterizedType = (ParameterizedType) getClass().getGenericSuperclass();
         return (Class<T>) parameterizedType.getActualTypeArguments()[0];
     }
+
+    // Fix to run the changes watcher in ThreadPool, so that they are alive - START
+
+    protected ListChangesAsyncTask couchChangesAsyncTask;
+
+    @Override
+    protected void updateListItems() {
+        if (updateListItemsTask == null) {
+
+            updateListItemsTask = new EktorpAsyncTask() {
+
+                protected ViewResult viewResult;
+
+                @Override
+                protected void doInBackground() {
+                    viewResult = couchDbConnector.queryView(viewQuery);
+                }
+
+                protected void onSuccess() {
+                    if (viewResult != null) {
+                        lastUpdateView = viewResult.getUpdateSeq();
+                        listRows = viewResult.getRows();
+                        notifyDataSetChanged();
+                    }
+                    updateListItemsTask = null;
+
+                    if (couchChangesAsyncTask == null && followChanges) {
+                        ChangesCommand changesCmd = new ChangesCommand.Builder().since(lastUpdateView)
+                                .includeDocs(false)
+                                .continuous(true)
+                                .heartbeat(5000)
+                                .build();
+
+                        couchChangesAsyncTask = new ListChangesAsyncTask(couchDbConnector, changesCmd);
+                        couchChangesAsyncTask.execute();
+                    }
+
+                    if (lastUpdateChangesFeed > lastUpdateView) {
+                        updateListItems();
+                    }
+
+                }
+
+                @Override
+                protected void onDbAccessException(
+                        DbAccessException dbAccessException) {
+                    handleViewAsyncTaskDbAccessException(dbAccessException);
+                }
+
+            };
+
+            updateListItemsTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        }
+    }
+
+    private class ListChangesAsyncTask extends ChangesFeedAsyncTask {
+
+        public ListChangesAsyncTask(CouchDbConnector couchDbConnector,
+                                    ChangesCommand changesCommand) {
+            super(couchDbConnector, changesCommand);
+        }
+
+        @Override
+        protected void handleDocumentChange(DocumentChange change) {
+            lastUpdateChangesFeed = change.getSequence();
+            updateListItems();
+        }
+
+        @Override
+        protected void onDbAccessException(DbAccessException dbAccessException) {
+            handleChangesAsyncTaskDbAccessException(dbAccessException);
+        }
+
+    }
+
+    // END
 
     abstract public void populateView(View view, T document);
 }
