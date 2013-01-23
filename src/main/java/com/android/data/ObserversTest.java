cb7d45e4ae227cfb547fbdc3f8ddd70e2ce48247
Selva <k.n.selvakumar@gmail.com>
2013-01-24 00:00:23 +0530

    Moving all the type reference identifier to util class onDelete notifier on document observer

--- src/main/java/com/android/data/ObserversTest.java
+++ src/main/java/com/android/data/ObserversTest.java
@@ -3,14 +3,19 @@ package com.android.data;
 import android.test.suitebuilder.annotation.MediumTest;
 import com.android.data.models.Task;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ObserversTest extends BaseTestCase {
     @Mock
     private ContentObserver<Task> contentObserver;
+    @Captor
+    private ArgumentCaptor<Task> taskCaptor;
 
     @Override
     public void setUp() throws Exception {
@@ -24,7 +29,6 @@ public class ObserversTest extends BaseTestCase {
         taskRepository.add(task);
         taskRepository.add(new Task("Another Doc"));
 
-        ArgumentCaptor<Task> taskCaptor = ArgumentCaptor.forClass(Task.class);
         taskRepository.registerDocumentObserver(task, contentObserver);
 
         task.updateDescription("Desc2");
@@ -33,5 +37,17 @@ public class ObserversTest extends BaseTestCase {
         sleepEnoughForChangesToBeFollowed();
         verify(contentObserver).onChange(taskCaptor.capture());
         assertEquals("Desc2", taskCaptor.getValue().getDescription());
+    }
+
+    @MediumTest
+    public void testFollowDeletes() throws InterruptedException {
+        Task task = new Task("Desc1");
+        taskRepository.add(task);
+
+        taskRepository.registerDocumentObserver(task, contentObserver);
+        taskRepository.remove(task);
+
+        sleepEnoughForChangesToBeFollowed();
+        verify(contentObserver).onDelete(eq(task.getId()), anyString());
     }
 }
