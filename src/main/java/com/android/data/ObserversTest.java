35ff2ea6d55f79406b3aa2156e7ad452d300b86d
Selva <k.n.selvakumar@gmail.com>
2013-01-25 19:42:14 +0530

    Moving back mock based test for observer to avoid randomness

--- src/main/java/com/android/data/ObserversTest.java
+++ src/main/java/com/android/data/ObserversTest.java
@@ -2,59 +2,58 @@ package com.android.data;
 
 import android.test.suitebuilder.annotation.MediumTest;
 import com.android.data.models.Task;
+import junit.framework.Assert;
+import org.ektorp.changes.ChangesCommand;
+import org.mockito.Mock;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ObserversTest extends BaseTestCase {
-    private boolean onChangeCalled;
-    private boolean documentDeleted;
-    private Task documentChanged;
-
-    ContentObserver<Task> taskObserver = new ContentObserver<Task>(){
-        @Override
-        public void onChange(Task document) {
-            documentChanged = document;
-            onChangeCalled = true;
-        }
-        @Override
-        public void onDelete(String docId, String revision) {
-            documentDeleted = true;
-        }
-    };
+    @Mock
+    private ContentObserver<Task> taskObserver;
+    private Task task;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
         initMocks(this);
-        onChangeCalled = false;
-        documentDeleted = false;
+        ChangesCommand changesCommand = taskChangesCommand();
+        task = new Task("Desc1");
+        given(taskObserver.commandToFollowChangesContinuously()).willReturn(changesCommand);
+        taskRepository.add(task);
+
     }
 
     @MediumTest
     public void testFollowChangesInDocument() throws InterruptedException {
-        Task task = new Task("Desc1");
-        taskRepository.add(task);
-        taskRepository.add(new Task("Another Doc"));
-
         taskRepository.registerDocumentObserver(task, taskObserver);
 
         task.updateDescription("Desc2");
         taskRepository.update(task);
 
         sleepEnoughForChangesToBeFollowed();
-        assertTrue(onChangeCalled);
-        assertEquals("Desc2", documentChanged.getDescription());
+        verify(taskObserver).onChange(task);
     }
 
     @MediumTest
     public void testFollowDeletes() throws InterruptedException {
-        Task task = new Task("Desc1");
-        taskRepository.add(task);
-
         taskRepository.registerDocumentObserver(task, taskObserver);
         taskRepository.remove(task);
 
         sleepEnoughForChangesToBeFollowed();
-        assertTrue(documentDeleted);
+        verify(taskObserver).onDelete(eq(task.getId()), anyString());
+    }
+
+    private ChangesCommand taskChangesCommand() {
+        return new ContentObserver<Task>(){
+            @Override
+            public void onChange(Task document) {
+                Assert.fail("Im a just a changes command generator. You should never see me!");
+            }
+        }.commandToFollowChangesContinuously();
     }
 }
